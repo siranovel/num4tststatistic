ac796d42bb76ef0eedfa3ed1436c24dfa1167fd0
siranovel <siranovel@gmail.com>
2023-11-12 11:17:11 +0900

    Update samplez.rb

--- sample/samplez.rb
+++ sample/samplez.rb
@@ -1,5 +1,30 @@
 require 'num4tststatistic'
-require './mymatcher.rb'
+# 小数点桁四捨五入
+module MyRoundMatcher
+  class Matcher
+    def initialize(expected, n)
+      @expected = expected
+      @n = n
+    end
+
+    def matches?(actual)
+      @actual = actual.round(@n)
+      @actual == @expected
+    end
+
+    def failure_message
+      "#{@expected} expected but got #{@actual}"
+    end
+  end
+  def my_round(expected, n)
+    Matcher.new(expected, n)
+  end
+end
+
+RSpec.configure do |config|
+  config.include MyRoundMatcher
+end
+
 
 
 RSpec.describe Num4TstStatisticLib do
