c05468ba20fac5f111ca06ff0ce1741be944b683
siranovel <siranovel@gmail.com>
2023-11-18 11:32:08 +0900

    Add files via upload

--- ext/num4tststatistic/TstStatistic.java
+++ ext/num4tststatistic/TstStatistic.java
@@ -172,6 +172,20 @@ public class TstStatistic {
 
         return t;
     }
+    public static double grubbs(double[] xi, double xk) {
+        SummaryStatistics stat = new SummaryStatistics();
+
+        Arrays.stream(xi).forEach(stat::addValue);
+        double m = stat.getMean();     // 平均
+        double sd = stat.getStandardDeviation();// 標準偏差
+        double min = stat.getMin();
+        double max = stat.getMax();
+        double t = 0.0;
+
+        if (xk == min) { t = (m - xk) / sd; }
+        if (xk == max) { t = (xk - m) / sd; }
+        return t;
+    }
 }
 
 
